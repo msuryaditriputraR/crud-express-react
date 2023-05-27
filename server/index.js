c4edd5dfd8a59649cad1596aab7aa00b11afc500
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-05-27 21:26:13 +0800

    add: routes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import cors from "cors";
 
+import UserRoute from "./routes/UserRoute.js";
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(UserRoute);
 
 app.listen(PORT, () => console.log(`Server has started on ${PORT}`));
