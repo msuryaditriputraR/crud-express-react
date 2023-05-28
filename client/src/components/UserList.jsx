051408c9b38e15d851512b6ea639bcfbfc6f9adc
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-05-28 14:01:44 +0800

    add: userlist

---
+++ client/src/components/UserList.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function UserList() {
+    const [users, setUsers] = useState([]);
+
+    const getUsers = async () => {
+        const response = await axios.get(
+            import.meta.env.VITE_SERVER + "/users"
+        );
+        setUsers(response.data);
+    };
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    return (
+        <section>
+            <div className="columns mt-5 is-centered">
+                <div className="column is-half">
+                    <table className="table is-striped is-fullwidth">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama</th>
+                                <th>Email</th>
+                                <th>Gender</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {users.map((user, index) => (
+                                <tr key={user.id}>
+                                    <td>{index + 1}</td>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.gender}</td>
+                                    <td>
+                                        <button className="button is-small mr-2 is-info">
+                                            Edit
+                                        </button>
+                                        <button className="button is-small  is-danger">
+                                            Delete
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </section>
+    );
+}
